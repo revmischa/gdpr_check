dc77c0146fc64082a789ac64b2b420564d8610ed
Mischa Spiegelmock <revmischa@cpan.org>
2018-05-04 07:33:54 -0700

    markdown readme

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='gdpr_check',
-    version='0.1',
+    version='0.2',
     description='Check if a user is covered by GDPR. Compare IP address to list of EEA/EU countries.',
     url='http://github.com/revmischa/gdpr_check',
     author='Mischa Spiegelmock',
     author_email='revmischa@cpan.org',
     license='ABRMS',
     packages=['gdpr_check'],
-    zip_safe=False
+    zip_safe=False,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['gdpr', 'eea', 'eu', 'ip', 'check', 'geoip', 'countrys'],
 )
